96cd4ec16aef0479bc4d2b8af53da58d36c5787d
Ahmed Abdul Rehman <ahmedabdulrehman2005@gmail.com>
2025-11-09 16:20:35 +0500

    Changes in Instructor file

--- Services/UserManagement/Classes/Instructor/Instructor.java
+++ Services/UserManagement/Classes/Instructor/Instructor.java
@@ -6,12 +6,12 @@ public class Instructor extends User {
     private String InstructorName;
 
     public Instructor() {
-        super("","","");
+        super("", "", "");
         this.InstructorName = "";
     }
 
     public Instructor(String userId, String userEmail, String userPass, String AdminName) {
-        super(userId,userEmail,userPass);
+        super(userId, userEmail, userPass);
         this.InstructorName = AdminName;
 
     }
@@ -25,42 +25,43 @@ public class Instructor extends User {
     public String GetUserId() {
         return super.GetUserId();
     };
-    
+
     @Override
     public String GetUserEmail() {
-        return super.GetUserEmail();    
+        return super.GetUserEmail();
     };
-    
+
     @Override
     public String GetUserPassword() {
-        return super.GetUserPassword();  
+        return super.GetUserPassword();
     };
-    
-    public String GetAdminName() {
+
+    public String GetInstructorName() {
         return this.InstructorName;
     }
-    
+
     @Override
     public void SetUserId(String userId) {
         super.SetUserId(userId);
     };
-    
+
     @Override
     public void SetUserEmail(String userEmail) {
         super.SetUserEmail(userEmail);
     };
-    
+
     @Override
     public void SetUserPassword(String userPassword) {
         super.SetUserPassword(userPassword);
     };
 
-    public void SetAdminName(String InstructorName) {
+    public void SetInstructorName(String InstructorName) {
         this.InstructorName = InstructorName;
     }
 
     @Override
     public String toString() {
-        return "The Instructor Name is " + this.InstructorName + " and the Instructor UserID is " + this.GetUserId() + " and Instructor Email is " + this.GetUserEmail();
+        return "The Instructor Name is " + this.InstructorName + " and the Instructor UserID is " + this.GetUserId()
+                + " and Instructor Email is " + this.GetUserEmail();
     }
 }
